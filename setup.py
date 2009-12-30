afdc3f66ff93cbfcbaa32b91790ba05609cb3047
Tim Abell <tim@timwise.co.uk>
2009-12-30 18:44:11 +0000

    setup.py added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup (name = "podpooch",
+	version = "0.2.0.3",
+	package_dir = {'': 'src'},
+	py_modules = ['podpooch', 'podpoochlib.playpod'],
+	packages = ["podpoochlib"],
+	url = "http://wiki.openmoko.org/wiki/PodPooch",
+	author = "Tim Abell et al",
+	author_email = "tim@timwise.co.uk",
+	maintainer = "Tim Abell",
+	maintainer_email = "tim@timwise.co.uk",
+	)
+
